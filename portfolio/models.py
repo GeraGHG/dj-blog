ac1f24929532a0e454e24cf53d4a82e2a8a9bbb2
GeraGHG <gera.GHGhernandez@gmail.com>
2023-12-20 14:40:11 -0700

    details in models

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField("Name project: ", max_length=100)
-    description = models.CharField("Description: ", max_length=500)
+    name = models.CharField("Name project", max_length=100)
+    description = models.CharField("Description", max_length=500)
     image = models.ImageField(upload_to="portfolio/images/")
     url = models.URLField(blank=True)
