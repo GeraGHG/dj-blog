866e46032f0ed80609e8d1815b2726c42220f6ea
GeraGHG <gera.GHGhernandez@gmail.com>
2023-12-21 14:36:10 -0700

    created view Home and template home.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Project
 
 # Create your views here.
+
+class Home(View):
+    def get(self, request, *args, **kwargs):
+            projects = Project.objects.all()
+            context = {
+                  'projects': projects
+            }
+            return render(request, 'home.html', context)
